7a32e0e15061ec5d7e56d8307a2fb05e2a8c7433
Mark Gable <mgable43@gmail.com>
2019-08-31 15:53:24 -0700

    Updates

---
+++ precache-manifest.622262a45520cc96c7fce09e574117aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76522b327cd6b04383cd0bf0ba0ea6ed",
+    "url": "/max_code_project/index.html"
+  },
+  {
+    "revision": "921976df18d0728791b5",
+    "url": "/max_code_project/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "aebfa4acf515a996adaa",
+    "url": "/max_code_project/static/css/main.eadc3029.chunk.css"
+  },
+  {
+    "revision": "921976df18d0728791b5",
+    "url": "/max_code_project/static/js/2.308f0ac0.chunk.js"
+  },
+  {
+    "revision": "aebfa4acf515a996adaa",
+    "url": "/max_code_project/static/js/main.2f102394.chunk.js"
+  },
+  {
+    "revision": "67413b448b1dddb6192e",
+    "url": "/max_code_project/static/js/runtime~main.4c3e488a.js"
+  }
+]);
